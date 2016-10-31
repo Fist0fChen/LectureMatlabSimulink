85f5b01cc24e6664de47679d89899d6c59ec33a5
Fist0fChen <jan_201@hotmail.de>
2016-10-31 17:20:00 +0100

    Exercise 3 plot functions

---
+++ Exercise_3_1.m
@@ -0,0 +1,18 @@
+%Exercise 3 as script
+Setup;
+
+y1 = @(x) x.^2;
+y2 = @(x) (exp(-x)/(x.^2 + 1) + sin(x).^2).^2 + 0.2;
+
+x = -1:0.01:1;
+
+figure('Name','Matlab Simulink Lecture Exercise 3','NumberTitle','Off')
+subplot(2,1,1)
+plot(x, y1(x))
+grid on
+title('y = x^2')
+
+subplot(2,1,2)
+plot(x, y2(x))
+grid on
+title('y = (exp(-x)/(x.^2 + 1) + sin(x).^2).^2 + 0.2')
